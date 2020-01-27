68b3ef1c387b79e4f1935c99eed63c995cbf2ab8
Alexis Panyathong <alexispanyathong@Alexiss-Air.lan>
2020-01-27 17:13:50 -0600

    Progress
    I completed the dll_queue.py and everything passed.

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,19 +1,31 @@
 import sys
-sys.path.append('../doubly_linked_list')
+sys.path.append('doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
-
+# Queue (cue)
 class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        # Each element has a reference to both the next and the previous element. This is useful bc having a reference to the previous element can speed up some operations, like removing (“unlinking”) an element from a list or traversing the list in reverse order.
+        
+        # We are setting DoublyLinkedList to storage
+        self.storage = DoublyLinkedList()
 
+    # Enqueue - to add (an item of data awaiting processing) to a queue of such items.
     def enqueue(self, value):
-        pass
+        # We are adding, so set the size to += 1.
+        self.size += 1
+        return self.storage.add_to_tail(value)
+        
 
     def dequeue(self):
-        pass
+        if self.size == 0:
+            pass
+        else:
+            self.size -= 1
+            return self.storage.remove_from_head()
+        
 
     def len(self):
-        pass
+        return self.size
